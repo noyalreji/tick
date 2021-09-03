199e366026304943d2e88d7e92815de3e40625dd
noyalreji <84338635+noyalreji@users.noreply.github.com>
2021-09-02 20:30:19 -0500

    Create hoisting.js

---
+++ JS_fundamentals/hoisting.js
@@ -0,0 +1,14 @@
+//1
+//console.log(hello);                                   
+//var hello = 'world'; 
+//console.log(hello);    //UNDEFINED                               
+//var hello = 'world'; //HELLO=WORLD
+
+
+
+var needle = 'haystack';
+test();
+function test(){
+	var needle = 'magnet';
+	console.log(needle);
+}

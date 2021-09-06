85fccc5b34ca932cca1d5ced073864a30f9b9087
noyalreji <84338635+noyalreji@users.noreply.github.com>
2021-09-05 22:35:34 -0500

    Create ajax.js

---
+++ ajax.js
@@ -0,0 +1,25 @@
+// assuming you have jQuery
+$(document).ready(function(){
+    var favoritePokemon;
+    // the given link is for Bulbasaur's information
+    $.get("https://pokeapi.co/api/v2/pokemon/1/", function(data){     
+        favoritePokemon = data.name;
+    });
+    console.log("Got my favorite Pokemon", favoritePokemon);      // Predict the output!
+});     
+
+// assuming you have jQuery
+$(document).ready(function(){
+    var favoritePokemon;
+    // the given link is for Bulbasaur's information
+    $.get("https://pokeapi.co/api/v2/pokemon/1/", function(data){ 
+    	favoritePokemon = data.name;
+	console.log("Got my favorite Pokemon", favoritePokemon);      // Predict the output!
+    });
+});    
+
+
+var secondFavorite;
+// let's use setTimeout to simulate an API call that takes 0 seconds to complete
+setTimeout(function(){ secondFavorite = "charmander"; }, 0);
+console.log("Got my second favorite", secondFavorite);

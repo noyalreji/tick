2b7a7055fa0a61ba74f29f0104360201258533b8
Noyal Reji Jacob <84338635+noyalreji@users.noreply.github.com>
2021-09-13 23:29:32 -0500

    Create es6_classes.js

---
+++ JS_fundamentals/es6_classes.js
@@ -0,0 +1,16 @@
+class Dot {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+//////////////////////////////////////////
+class Dot {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        console.log("You created a Dot!");
+    }
+}
+const dot1 = new Dot(10, 10);
+//////////////////////////////////////////

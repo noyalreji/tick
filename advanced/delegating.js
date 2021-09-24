7facdb39f988dc6639913bda6835dbd3ccddc76a
Noyal Reji Jacob <84338635+noyalreji@users.noreply.github.com>
2021-09-23 22:14:05 -0500

    Update delegating.js

--- advanced/delegating.js
+++ advanced/delegating.js
@@ -55,4 +55,5 @@ function printResult(doSomething) {
  var result = doSomething();         // store the return value of the callback parameter
  console.log(result);                // print the result!
 }
+printResult(function returnFive(){ return 5 })  // this should print "5"
 
